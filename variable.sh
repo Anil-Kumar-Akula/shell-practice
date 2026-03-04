ea43a238eb7ee0ec42b2bf7186f9e15b16f027da
Anil Kumar <as.anil@OFFISERV.LOCAL>
2026-03-04 11:15:33 +0530

    vars

--- variable.sh
+++ variable.sh
@@ -3,5 +3,5 @@
 PERSON1= "Anil kumar"
 PERSON2= "Allu Arjun"
 
-$PERSON1 :: Hello $PERSON2 , how are you..?
-$PERSON2 :: yeah, Iam doing good $PERSON2
+$PERSON1 :: "Hello $PERSON2 , how are you..? "
+$PERSON2 :: " yeah, Iam doing good $PERSON2 "
