38d422a863100732e4e256489956b8e55ea6f1cb
Anil Kumar <as.anil@OFFISERV.LOCAL>
2026-03-05 16:33:42 +0530

    vars

--- data-types.sh
+++ data-types.sh
@@ -6,6 +6,6 @@ NUMBER2=200
 SUM=$(($NUMBER1+$NUMBER2))
 echo "Total value :: $SUM"
 
-CRICKETERS=("sachin","yuvaraj","dhoni","shewag")
+CRICKETERS=("sachin" "yuvaraj" "dhoni" "shewag")
 
-echo "NO OF PLAYERS :: $CRICKETERS"
+echo "NO OF CRICKET PLAYERS :: $CRICKETERS"
