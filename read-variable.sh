0b37d3711c2c56efb1588e7707cad8acf8b30d9f
Anil Kumar <as.anil@OFFISERV.LOCAL>
2026-03-05 14:57:22 +0530

    vars

--- read-variable.sh
+++ read-variable.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Please enter your name"
-read name
+# echo "Please enter your name"
+# read name
 
-echo "Please enter your pin"
-read -s pin
+# echo "Please enter your pin"
+# read -s pin
 
-echo "name entered as :: $name"
-echo "pin entered as :: $pin"
+# echo "name entered as :: $name"
+# echo "pin entered as :: $pin"
 
 DATE=$(date)
 echo $DATE
