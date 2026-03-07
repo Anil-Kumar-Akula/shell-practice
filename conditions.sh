ca14ff8f556538c7ad9c6824929535484beef1d8
Anil Kumar <as.anil@OFFISERV.LOCAL>
2026-03-07 11:01:21 +0530

    cond

--- conditions.sh
+++ conditions.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-SEASON=rain
+# SEASON=rain
 
-if [ "$SEASON" = "rain" ]; then
-    echo "using the umbrella because of rain"
+# if [ "$SEASON" = "rain" ]; then
+#     echo "using the umbrella because of rain"
 
-else 
-  echo "using the sun glasses dues to high temperature"
-fi
+# else 
+#   echo "using the sun glasses dues to high temperature"
+# fi
+
+NUMBER=5
+
+if [$NUMBER > 10 ]; then
+ echo "$NUMBER is not greater than 10"
+else
+ echo "$NUMBER is greater than 10"
