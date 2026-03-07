0d41636fdc92c8c8c0748e40b657854ef938dc5d
Anil Kumar <as.anil@OFFISERV.LOCAL>
2026-03-07 11:19:50 +0530

    cond

--- conditions.sh
+++ conditions.sh
@@ -12,6 +12,8 @@ NUMBER=100
 
 if [  $NUMBER -gt 10 ]; then
  echo "$NUMBER is not greater than 10"
+elif [ $NUMBER -eq 10 ]; then
+ echo "$NUMBER is equal to 10"
 else
  echo "$NUMBER is greater than 10"
 fi
