29485e894ae04219e28b1defc7d35fc0f53af36f
Anil Kumar <as.anil@OFFISERV.LOCAL>
2026-03-07 11:30:00 +0530

    cond

--- conditions.sh
+++ conditions.sh
@@ -8,12 +8,20 @@
 #   echo "using the sun glasses dues to high temperature"
 # fi
 
-NUMBER=$1
+# NUMBER=$1
 
-if [  $NUMBER -lt 10 ]; then
- echo "$NUMBER is less than 10"
-elif [ $NUMBER -eq 10 ]; then
- echo "$NUMBER is equal to 10"
+# if [  $NUMBER -lt 10 ]; then
+#  echo "$NUMBER is less than 10"
+# elif [ $NUMBER -eq 10 ]; then
+#  echo "$NUMBER is equal to 10"
+# else
+#  echo "$NUMBER is greater than 10"
+# fi
+
+EVEN=$1
+
+if [ $EVEN * 2 -eq 0 ]; then
+ echo "$EVEN is a even number"
 else
- echo "$NUMBER is greater than 10"
+ echo "$EVEN is a odd number"
 fi
