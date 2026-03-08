b40544b67f5f675b5a2ae9d953f3d7947c692d05
Anil Kumar <anilroyal8999@gmail.com>
2026-03-08 12:08:25 +0530

    script files

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-    echo "you are not using the root user previleges, please switch to the root user"
+    echo "ERROR :: you are not using the root user previleges, please switch to the root user"
 fi
 
 dnf install mysql -y
@@ -11,6 +11,6 @@ dnf install mysql -y
 if [ $? -eq 0 ]; then
     echo "mysql is installed successfully"
 else
-    echo "mysql installation failed"
+    echo "ERROR :: mysql installation failed"
 fi
 
