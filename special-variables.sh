5f6cb51309b8b8a9211e236a1b96b641011456fe
Anil Kumar <anilroyal8999@gmail.com>
2026-03-08 15:47:23 +0530

    script files

---
+++ special-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "all variables passed to the script : $@"
+echo "all variables passed to the script : $*"
+echo "total number of variables passed to the script : $#"
+echo "name of the script : $0"
+echo "first variable passed to the script : $1"
+echo "current directory : $PWD"
