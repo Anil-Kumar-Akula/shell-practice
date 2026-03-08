54c9bf9e4316b776e9f8a21b8f9ef71723972ba0
Anil Kumar <anilroyal8999@gmail.com>
2026-03-08 12:26:05 +0530

    script files

---
+++ installation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+packages=("mysql" "mongodb-mongosh" "nginx")
+
+for package in ${packages[@]}; do
+    dnf install $package -y
+    if [ $? -eq 0 ]; then
+        echo "$package is installed successfully"
+    else
+        echo "ERROR :: $package installation failed"
+        exit 1
+    fi
+done
