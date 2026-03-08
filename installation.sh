da519cfa62dba21a47a9b46b422850de2760e678
Anil Kumar <anilroyal8999@gmail.com>
2026-03-08 13:34:05 +0530

    script files

--- installation.sh
+++ installation.sh
@@ -14,28 +14,33 @@
 
 #!/bin/bash
 
-echo "Please Enter the Packages which you want to install"
+echo "please enter the install packages"
 read PACKAGES
 
-for i in ${PACKAGES[@]};do
-    if [ $(id -u) -ne 0 ]; then
-     echo "ERROR :: RUN AS ROOT USER"
-     exit 1
-    fi
 
-   rpm -q $i
-   
-    if [ $? -eq 0 ]; then
-     echo "$i are already installed in the system"
-    else
-     dnf install $i -y
+if [ $(id -u) -ne 0 ]; then
+
+ echo "ERROR :: Please use the root user privilege"
+
+fi
+
+for i in ${PACKAGES[@]}; do
+  
+  rpm -q $i
+  if [ $? -eq 0 ]; then
+    echo "$i already installed in the system"
     
-    
+  else
+        dnf install $i -y
+
         if [ $? -eq 0 ]; then
-         echo "$i are installed sucessfully"
+        
+          echo "$i installed successfully"
+        
         else
-         echo "ERROR :: $i installation failed"
-         exit 1
+        
+          echo "ERROR :: $i installation failed"
         fi
-    fi
+  fi
 done
+    
