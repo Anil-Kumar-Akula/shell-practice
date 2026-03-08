b3299cd7bc98158fb9dcb943c3d81620c66fc0c8
Anil Kumar <anilroyal8999@gmail.com>
2026-03-08 13:11:19 +0530

    script files

--- installation.sh
+++ installation.sh
@@ -29,13 +29,13 @@ for i in ${PACKAGES[@]};do
      echo "$i are already installed in the system"
     else
      dnf install $i -y"
-    fi
     
-    if [ $? -eq 0 ]; then
-     echo "$i are installed sucessfully"
-    else
-     echo "ERROR :: $i installation failed"
-     exit 1
+    
+        if [ $? -eq 0 ]; then
+        echo "$i are installed sucessfully"
+        else
+        echo "ERROR :: $i installation failed"
+        exit 1
+        fi
     fi
-
 done
