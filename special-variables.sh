5a9c141ee4682ea214e8ae3420c60eeb90623dde
Anil Kumar <anilroyal8999@gmail.com>
2026-03-08 15:50:07 +0530

    script files

--- special-variables.sh
+++ special-variables.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-echo "all variables passed to the script : $@"
-echo "all variables passed to the script : $*"
-echo "total number of variables passed to the script : $#"
-echo "name of the script : $0"
-echo "first variable passed to the script : $1"
-echo "current directory : $PWD"
+
+echo "All variables passed to the script: $@"
+echo "All variables passed to the script: $*"
+echo "Script name: $0"
+echo "Current directory: $PWD"
+echo "Who is running this: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of this script: $$"
+sleep 50 &
+echo "PID of the last command in background is: $!"
