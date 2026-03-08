1f0010927543356beb00a6b5436d31fad5baf22c
Anil Kumar <anilroyal8999@gmail.com>
2026-03-08 12:46:24 +0530

    script files

--- script.sh
+++ script.sh
@@ -1,18 +1,38 @@
+# #!/bin/bash
+
+# USERID=$(id -u)
+
+# if [ $USERID -ne 0 ]; then
+#     echo "ERROR :: you are not using the root user previleges, please switch to the root user"
+#     exit 1 # becasue 1 is the failure and 0 is success
+# fi
+
+# dnf install mysql -y
+
+# if [ $? -eq 0 ]; then
+#     echo "mysql is installed successfully"
+# else
+#     echo "ERROR :: mysql installation failed"
+#     exit 1 # failure is otherthan 0
+# fi
+
 #!/bin/bash
 
-USERID=$(id -u)
+echo "Please Enter the Packages which you want to install"
+read PACKAGES
 
-if [ $USERID -ne 0 ]; then
-    echo "ERROR :: you are not using the root user previleges, please switch to the root user"
-    exit 1 # becasue 1 is the failure and 0 is success
-fi
+for i in ${PACKAGES[@]};do
+    if [ $(id -u) -ne 0 ]; then
+     echo "ERROR :: RUN AS ROOT USER"
+     exit 1
+    fi
 
-dnf install mysql -y
-
-if [ $? -eq 0 ]; then
-    echo "mysql is installed successfully"
-else
-    echo "ERROR :: mysql installation failed"
-    exit 1 # failure is otherthan 0
-fi
-
+    dnf install $i -y
+   
+    if [ $? -eq 0 ]; then
+     echo "$i are installed successful"
+    else
+     echo "ERROR :: $i are not installed sucessfull"
+     exit 1
+    fi
+done
