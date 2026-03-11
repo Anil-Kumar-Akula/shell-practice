33ca57d7b5412b67d9958854577eeb5634ec8205
Anil Kumar <anilroyal8999@gmail.com>
2026-03-11 22:05:34 +0530

    all

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-org -y 
-systemctl enable mongod 
-systemctl start mongod 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-systemctl restart mongod
+
+if [ $(id -u) -ne 0 ]; then
+    echo "ERROR :: RUN AS ROOT USER"
+    exit 1
+else
+    cp mongo.repo /etc/yum.repos.d/mongo.repo
+    
+    dnf install mongodb-org -y 
+    if [$? -eq 0]; then
+       echo "mongodb enabling"
+       systemctl enable mongod
+       
+    else 
+       echo "mongo db enable failed"
+
+    fi
+    systemctl start mongod 
+    sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+    systemctl restart mongod
+
+fi
