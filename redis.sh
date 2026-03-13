018ea250e5be457b18dd9089d2baae5ed14af2f5
Anil Kumar <anilroyal8999@gmail.com>
2026-03-13 19:54:08 +0530

    redis file

---
+++ redis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+dnf module disable redis -y
+dnf module enable redis:7 -y
+dnf install redis -y 
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf
+sed -i 's/protected-mode yes/protected-mode no/g' /etc/redis.conf
+systemctl enable redis
+systemctl start redis
